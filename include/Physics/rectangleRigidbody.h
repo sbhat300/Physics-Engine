55f4932bc324694faa22a32b7d13e13a9688ab9a
Terra101 <sbhatkande100@gmail.com>
2024-02-24 23:33:27 -0500

    friction

--- include/Physics/rectangleRigidbody.h
+++ include/Physics/rectangleRigidbody.h
@@ -12,11 +12,12 @@ class rectangleRigidbody : public rectangleCollider
         glm::vec3 force, velocity, impulse;
         float mass;
         float invMass;
+        float staticFriction, dynamicFriction;
         std::map<int, rectangleRigidbody*>* collidables;
         float restitution;
 
         rectangleRigidbody(float h = 1, float w = 1, float r = 0, glm::vec3 s = glm::vec3(1, 1, 1), glm::vec3 p = glm::vec3(0, 0, 0), 
-        float* dt = 0, int* counter = 0, std::map<int, rectangleCollider*>* cs = 0, float m = 0, std::map<int, rectangleRigidbody*>* rbs = 0, float res = 0) 
+        float* dt = 0, int* counter = 0, std::map<int, rectangleCollider*>* cs = 0, float m = 0, std::map<int, rectangleRigidbody*>* rbs = 0, float res = 0, float muS = 0, float muK = 0) 
         : rectangleCollider(h, w, r, s, p, dt, counter, cs)
         {
             mass = m;
@@ -28,12 +29,14 @@ class rectangleRigidbody : public rectangleCollider
             restitution = res;
             if(mass == 0) invMass = 0;
             else invMass = 1 / mass;
+            staticFriction = muS;
+            dynamicFriction = muK;
         }
         void updateRigidbody()
         {
             previousPos = glm::vec3(position.x, position.y, 0);
             velocity += force * invMass * *deltaTime + impulse * invMass;
-            position += velocity;
+            position += velocity * *deltaTime;
             force = glm::vec3(0, 0, 0);
             impulse = glm::vec3(0, 0, 0);
             updatePoints();
